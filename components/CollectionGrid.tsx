65fda0a763cd47114e87ad354ee719d84caef24a
Chentao <coolpixeltyler@gmail.com>
2025-01-22 02:17:24 -0600

    update

--- components/CollectionGrid.tsx
+++ components/CollectionGrid.tsx
@@ -9,7 +9,6 @@ export const CollectionGrid = ({ collections }: { collections: Collection[] }) =
           key={collection.id}
           className="flex flex-col items-center border border-gray-300 rounded-lg bg-[#F0F0F5] shadow hover:border-gray-400 cursor-pointer w-[20vw] py-8"
         >
-          {/* Icon */}
           <div className="w-12 h-12 mb-2">
             <Image
               src={collection.icon}
@@ -20,7 +19,6 @@ export const CollectionGrid = ({ collections }: { collections: Collection[] }) =
             />
           </div>
 
-          {/* Text */}
           <div className="text-center">
             <h3 className="text-lg font-semibold">{collection.name}</h3>
             <p className="text-sm text-gray-500">{collection.created}</p>
